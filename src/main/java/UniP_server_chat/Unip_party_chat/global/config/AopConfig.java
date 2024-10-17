1814ca3fd212814fe7b22a35007e000b162c51a7
이경훈 <100902332+lgh000322@users.noreply.github.com>
2024-10-17 14:16:12 +0900

    Chore: config 수정

--- src/main/java/UniP_server_chat/Unip_party_chat/global/config/AopConfig.java
+++ src/main/java/UniP_server_chat/Unip_party_chat/global/config/AopConfig.java
@@ -3,6 +3,7 @@ package UniP_server_chat.Unip_party_chat.global.config;
 import UniP_server_chat.Unip_party_chat.global.logTrace.aspect.FilterResponseAspect;
 import UniP_server_chat.Unip_party_chat.global.logTrace.aspect.LogTraceAspect;
 import UniP_server_chat.Unip_party_chat.global.logTrace.material.LogTrace;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -16,7 +17,7 @@ public class AopConfig {
     }
 
     @Bean
-    public FilterResponseAspect filterResponseAspect() {
-        return new FilterResponseAspect();
+    public FilterResponseAspect filterResponseAspect(ObjectMapper objectMapper) {
+        return new FilterResponseAspect(objectMapper);
     }
 }
