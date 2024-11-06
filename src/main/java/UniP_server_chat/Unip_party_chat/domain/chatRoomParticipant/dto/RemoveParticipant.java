49c3736e41864dcccf3f950ef6ad41b2e3367f82
이경훈 <100902332+lgh000322@users.noreply.github.com>
2024-11-06 22:52:36 +0900

    Fix: validator 수정

--- src/main/java/UniP_server_chat/Unip_party_chat/domain/chatRoomParticipant/dto/RemoveParticipant.java
+++ src/main/java/UniP_server_chat/Unip_party_chat/domain/chatRoomParticipant/dto/RemoveParticipant.java
@@ -2,13 +2,14 @@ package UniP_server_chat.Unip_party_chat.domain.chatRoomParticipant.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.util.UUID;
 
 @Builder
 @Schema(description = "제거할 채팅 참여자 DTO")
-public record RemoveParticipant(@NotEmpty(message = "방 번호는 필수입니다.")
+public record RemoveParticipant(@NotNull(message = "방 번호는 필수입니다.")
                                 @Schema(description = "방 번호", example = "10") UUID roomId) {
 
 }
