1d18fa8cbc6c045fae0139e7ff31f750d54268c0
이경훈 <100902332+lgh000322@users.noreply.github.com>
2024-11-17 16:00:04 +0900

    Chore: 시간데이터 응답 형식 변경

--- src/main/java/UniP_server_chat/Unip_party_chat/domain/chatRoom/dto/ChatRoomsDto.java
+++ src/main/java/UniP_server_chat/Unip_party_chat/domain/chatRoom/dto/ChatRoomsDto.java
@@ -2,6 +2,7 @@ package UniP_server_chat.Unip_party_chat.domain.chatRoom.dto;
 
 
 import UniP_server_chat.Unip_party_chat.domain.party.entity.consts.PartyType;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -11,8 +12,8 @@ import java.util.UUID;
 public record ChatRoomsDto(UUID id,
                            String title,
                            PartyType partyType,
-                           LocalDateTime startTime,
-                           LocalDateTime endTime,
+                           @JsonFormat(pattern = "yyyy.MM.dd/HH") LocalDateTime startTime,
+                           @JsonFormat(pattern = "yyyy.MM.dd/HH") LocalDateTime endTime,
                            String partyChiefName,
                            String partyChiefImageUrl,
                            Long nowCounted,
